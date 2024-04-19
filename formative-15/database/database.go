2ea7988c68c489236ca7f0a29201648dd4380e50
regaagassi22 <82117812+regaagassi22@users.noreply.github.com>
2024-04-20 06:10:35 +0700

    Add files via upload

---
+++ formative-15/database/database.go
@@ -0,0 +1,31 @@
+package database
+
+import (
+	"database/sql"
+	"fmt"
+
+	"embed"
+
+	migrate "github.com/rubenv/sql-migrate"
+)
+
+//go:embed sql_migrations/*.sql
+var dbMigrations embed.FS
+var (
+	DbConnection *sql.DB
+)
+
+func DbMigrate(dbParam *sql.DB) {
+	migrations := &migrate.EmbedFileSystemMigrationSource{
+		FileSystem: dbMigrations,
+		Root:       "sql_migrations",
+	}
+
+	n, errs := migrate.Exec(dbParam, "postgres", migrations, migrate.Up)
+	if errs != nil {
+		panic(errs)
+	}
+
+	DbConnection = dbParam
+	fmt.Println("Applied", n, " migrations!")
+}
